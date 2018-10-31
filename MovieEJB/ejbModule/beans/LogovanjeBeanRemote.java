848d44e9bc3907d38539b1bf90a59e69503a17cb
jovicdragoslav <thejovic@icloud.com>
2018-10-31 01:39:50 +0100

    Update MovieEJB/ejbModule/beans/LogovanjeBeanRemote.java

---
+++ MovieEJB/ejbModule/beans/LogovanjeBeanRemote.java
@@ -0,0 +1,16 @@
+package beans;
+
+import javax.ejb.Remote;
+
+import model.Korisnik;
+
+@Remote
+public interface LogovanjeBeanRemote {
+	public String login(String username, String password);
+	public boolean registracija(String email, String imeKorisnika, String password, String prezimeKorisnika, String uloga, String username);
+	public void justContinue();
+	public void logout();
+	public Korisnik getLoggedUser();
+	public void setLoggedUser(Korisnik loggedUser);
+	public boolean sacuvajOcenu(int idkor, int idproj, int ocena);
+}
