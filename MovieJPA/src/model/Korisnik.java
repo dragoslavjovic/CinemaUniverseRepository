37968530c4eb3840b07daa3fdab0ff213eb4e964
jovicdragoslav <thejovic@icloud.com>
2018-10-31 01:43:24 +0100

    Update MovieJPA/src/model/Korisnik.java

---
+++ MovieJPA/src/model/Korisnik.java
@@ -0,0 +1,171 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the TIM8KORISNIK database table.
+ * 
+ */
+@Entity
+@Table(name="TIM8KORISNIK")
+@NamedQuery(name="Korisnik.findAll", query="SELECT k FROM Korisnik k")
+public class Korisnik implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int korisnikID;
+
+	private String email;
+
+	private String imeKorisnika;
+
+	private String password;
+
+	private String prezimeKorisnika;
+
+	private String uloga;
+
+	private String username;
+
+	//bi-directional many-to-one association to Komentar
+	@OneToMany(mappedBy="tim8korisnik")
+	private List<Komentar> tim8komentars;
+
+	//bi-directional many-to-one association to ProjekcijaKorisnik
+	@OneToMany(mappedBy="tim8korisnik")
+	private List<ProjekcijaKorisnik> tim8projekcijakorisniks;
+
+	//bi-directional many-to-one association to Rezervacija
+	@OneToMany(mappedBy="tim8korisnik")
+	private List<Rezervacija> tim8rezervacijas;
+
+	public Korisnik() {
+	}
+
+	public int getKorisnikID() {
+		return this.korisnikID;
+	}
+
+	public void setKorisnikID(int korisnikID) {
+		this.korisnikID = korisnikID;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getImeKorisnika() {
+		return this.imeKorisnika;
+	}
+
+	public void setImeKorisnika(String imeKorisnika) {
+		this.imeKorisnika = imeKorisnika;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPrezimeKorisnika() {
+		return this.prezimeKorisnika;
+	}
+
+	public void setPrezimeKorisnika(String prezimeKorisnika) {
+		this.prezimeKorisnika = prezimeKorisnika;
+	}
+
+	public String getUloga() {
+		return this.uloga;
+	}
+
+	public void setUloga(String uloga) {
+		this.uloga = uloga;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public List<Komentar> getTim8komentars() {
+		return this.tim8komentars;
+	}
+
+	public void setTim8komentars(List<Komentar> tim8komentars) {
+		this.tim8komentars = tim8komentars;
+	}
+
+	public Komentar addTim8komentar(Komentar tim8komentar) {
+		getTim8komentars().add(tim8komentar);
+		tim8komentar.setTim8korisnik(this);
+
+		return tim8komentar;
+	}
+
+	public Komentar removeTim8komentar(Komentar tim8komentar) {
+		getTim8komentars().remove(tim8komentar);
+		tim8komentar.setTim8korisnik(null);
+
+		return tim8komentar;
+	}
+
+	public List<ProjekcijaKorisnik> getTim8projekcijakorisniks() {
+		return this.tim8projekcijakorisniks;
+	}
+
+	public void setTim8projekcijakorisniks(List<ProjekcijaKorisnik> tim8projekcijakorisniks) {
+		this.tim8projekcijakorisniks = tim8projekcijakorisniks;
+	}
+
+	public ProjekcijaKorisnik addTim8projekcijakorisnik(ProjekcijaKorisnik tim8projekcijakorisnik) {
+		getTim8projekcijakorisniks().add(tim8projekcijakorisnik);
+		tim8projekcijakorisnik.setTim8korisnik(this);
+
+		return tim8projekcijakorisnik;
+	}
+
+	public ProjekcijaKorisnik removeTim8projekcijakorisnik(ProjekcijaKorisnik tim8projekcijakorisnik) {
+		getTim8projekcijakorisniks().remove(tim8projekcijakorisnik);
+		tim8projekcijakorisnik.setTim8korisnik(null);
+
+		return tim8projekcijakorisnik;
+	}
+
+	public List<Rezervacija> getTim8rezervacijas() {
+		return this.tim8rezervacijas;
+	}
+
+	public void setTim8rezervacijas(List<Rezervacija> tim8rezervacijas) {
+		this.tim8rezervacijas = tim8rezervacijas;
+	}
+
+	public Rezervacija addTim8rezervacija(Rezervacija tim8rezervacija) {
+		getTim8rezervacijas().add(tim8rezervacija);
+		tim8rezervacija.setTim8korisnik(this);
+
+		return tim8rezervacija;
+	}
+
+	public Rezervacija removeTim8rezervacija(Rezervacija tim8rezervacija) {
+		getTim8rezervacijas().remove(tim8rezervacija);
+		tim8rezervacija.setTim8korisnik(null);
+
+		return tim8rezervacija;
+	}
+
+}
